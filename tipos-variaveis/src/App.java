c13b9fe548a5a2b140ce845e129b31e24335a720
baaguimaraes <guimaraes_barbara@sicredi.com.br>
2024-06-24 17:20:01 -0300

    novas classes

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double valorPi = 3.14;
+        //com o final no comeco o valor não será alterado 
+    }
+}
---
+++ tipos-variaveis/src/operadores/operadoresTernarios.java
@@ -0,0 +1,20 @@
+package operadores;
+
+public class operadoresTernarios {
+    
+    public static void main(String [] args) {
+        int a, b;
+        a= 6;
+        b= 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+    
+     System.out.println(resultado);
+
+     
+     int resultado2 = a == b ? 1 : 0;
+    
+     System.out.println(resultado2);
+
+    }
+}
